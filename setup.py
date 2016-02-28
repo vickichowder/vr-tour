09d82ad307661cbb6352ef60c51826100506bb96
vickichowder <vechow12@gmail.com>
2016-02-28 03:05:02 -0800

    move everything up a folder

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+setup(
+    name     = 'tour it app',
+    version  = '1.0.0',
+    url      = 'https://github.com/vickichowder/vr-tour',
+    keywords = 'Touring with a local in VR',
+
+)

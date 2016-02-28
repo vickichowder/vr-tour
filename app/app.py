b582585d6db6c4b0d4b4bab812825b50eeaa36b4
vickichowder <vechow12@gmail.com>
2016-02-27 23:50:06 -0800

    login page

--- app/app.py
+++ app/app.py
@@ -10,74 +10,76 @@ database = 'dev'
 port = '1433'
 
 conn = pymssql.connect(server, user, password, database, port)
-cursor = conn.cursor()
+try:
+    cursor = conn.cursor()
+    print('connected to database')
+except:
+    print('did not connect to database')
 
 # try:
-# 	cursor.executemany(
+#     cursor.executemany(
 #     "INSERT INTO tourist VALUES ('name', 'phone', 'street_number', 'street_name', 'city', 'state', 'country', 'username', 'password')",
 #     [('Bob The Tourist', '4160983878', '34', 'University Avenue', 'Sunnyvale', 'CA', 'US', 'bobby', 'bobby')])
-# 	conn.commit()
-# 	print('inserted into table')
-# 	cursor.execute('select * from tourist;')
+#     conn.commit()
+#     print('inserted into table')
+#     cursor.execute('select * from tourist;')
 # except:
-# 	print('did not insert')
+#     print('did not insert')
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
 def main():
-	return render_template('home.html')
+    return render_template('home.html')
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
+		password = cursor.execute('select password from tourist where username=%s', request.form['username'])
+        if request.form['password'] != password.fetchone()[0]:
+            error = 'Invalid Credentials. Please try again.'
         else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
+            return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
 @app.route("/home")
 def signUp():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route("/signup")
 def signup():
-	return render_template('signup.html')
+    return render_template('signup.html')
 
 @app.route("/destinations")
 def destinations():
-	return render_template('destinations.html')
+    return render_template('destinations.html')
 
 @app.route("/requests")
 def requests():
-	return render_template('requests.html')
+    return render_template('requests.html')
 
 @app.route("/tourguidelist")
 def tourguidelist():
-	return render_template('tourguidelist.html')
+    return render_template('tourguidelist.html')
 
 @app.route("/starttour")
 def starttour():
-	return render_template('starttour.html')
+    return render_template('starttour.html')
 
 
 @app.route("/inprogress")
 def inprogress():
-	return render_template('inprogress.html')
+    return render_template('inprogress.html')
 
 @app.route("/endtour")
 def endtour():
-	return render_template('endtour.html')
+    return render_template('endtour.html')
 
 @app.route("/billingpage")
 def billingpage():
-	return render_template('billingpage.html')
+    return render_template('billingpage.html')
 
 if __name__ == "__main__":
-	app.run(port=8000)
+    app.run(port=8000)

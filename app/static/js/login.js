9761c0f14c235cddeb49236d2baf56f64fc74083
vickichowder <vechow12@gmail.com>
2016-02-27 23:01:05 -0800

    split up login and signup js file

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -17,8 +17,8 @@
 	// Validation
   $("#login-form").validate({
   	rules: {
-      lg_username: "required",
-  	  lg_password: "required",
+      username: "required",
+  	  password: "required",
     },
   	errorClass: "form-invalid"
   });
@@ -39,79 +39,7 @@
 		}
   });
 
-	// Register Form
-	//----------------------------------------------
-	// Validation
-  $("#register-form").validate({
-  	rules: {
-      reg_username: "required",
-  	  reg_password: {
-  			required: true,
-  			minlength: 5
-  		},
-   		reg_password_confirm: {
-  			required: true,
-  			minlength: 5,
-  			equalTo: "#register-form [name=reg_password]"
-  		},
-  		reg_email: {
-  	    required: true,
-  			email: true
-  		},
-  		reg_agree: "required",
-    },
-	  errorClass: "form-invalid",
-	  errorPlacement: function( label, element ) {
-	    if( element.attr( "type" ) === "checkbox" || element.attr( "type" ) === "radio" ) {
-    		element.parent().append( label ); // this would append the label after all your checkboxes/labels (so the error-label will be the last element in <div class="controls"> )
-	    }
-			else {
-  	  	label.insertAfter( element ); // standard behaviour
-  	  }
-    }
-  });
 
-  // Form Submission
-  $("#register-form").submit(function() {
-  	remove_loading($(this));
-
-		if(options['useAJAX'] == true)
-		{
-			// Dummy AJAX request (Replace this with your AJAX code)
-		  // If you don't want to use AJAX, remove this
-  	  dummy_submit_form($(this));
-
-		  // Cancel the normal submission.
-		  // If you don't want to use AJAX, remove this
-  	  return false;
-		}
-  });
-
-	// Forgot Password Form
-	//----------------------------------------------
-	// Validation
-  $("#forgot-password-form").validate({
-  	rules: {
-      fp_email: "required",
-    },
-  	errorClass: "form-invalid"
-  });
-
-	// Form Submission
-  $("#forgot-password-form").submit(function() {
-  	remove_loading($(this));
-
-		if(options['useAJAX'] == true)
-		{
-			// Dummy AJAX request (Replace this with your AJAX code)
-		  // If you don't want to use AJAX, remove this
-  	  dummy_submit_form($(this));
-
-		  // Cancel the normal submission.
-		  // If you don't want to use AJAX, remove this
-  	  return false;
-		}
-  });
 
 	// Loading
 	//----------------------------------------------
@@ -152,5 +80,4 @@
   		}, 2000);
   	}
   }
-
-})(jQuery);
+});
--- app/static/js/signup.js
+++ app/static/js/signup.js
@@ -1,16 +1,46 @@
-$(function(){
-	$('#btnSignUp').click(function(){
+// Register Form
+//----------------------------------------------
+// Validation
+$("#register-form").validate({
+  rules: {
+    username: "required",
+    password: {
+      required: true,
+      minlength: 5
+    },
+    password_confirm: {
+      required: true,
+      equalTo: "#register-form [name=reg_password]"
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    agree: "required",
+  },
+  errorClass: "form-invalid",
+  errorPlacement: function( label, element ) {
+    if( element.attr( "type" ) === "checkbox" || element.attr( "type" ) === "radio" ) {
+      element.parent().append( label ); // this would append the label after all your checkboxes/labels (so the error-label will be the last element in <div class="controls"> )
+    }
+    else {
+      label.insertAfter( element ); // standard behaviour
+    }
+  }
+});
 
-		$.ajax({
-			url: '/signUp',
-			data: $('form').serialize(),
-			type: 'POST',
-			success: function(response){
-				console.log(response);
-			},
-			error: function(error){
-				console.log(error);
-			}
-		});
-	});
+// Form Submission
+$("#register-form").submit(function() {
+  remove_loading($(this));
+
+  if(options['useAJAX'] == true)
+  {
+    // Dummy AJAX request (Replace this with your AJAX code)
+    // If you don't want to use AJAX, remove this
+    dummy_submit_form($(this));
+
+    // Cancel the normal submission.
+    // If you don't want to use AJAX, remove this
+    return false;
+  }
 });
